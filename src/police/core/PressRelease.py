1de977e68e7aeb4edba4e342e44392f9cda35405
nuuuwan <nuuuwan@gmail.com>
2025-08-29 10:19:30 +0530

    Initial empty commit

---
+++ src/police/core/PressRelease.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PressRelease:
+    time: str
+    url_pdf: str
---
+++ src/police/pages/PagePR.py
@@ -0,0 +1,69 @@
+from utils import Log
+
+from police.core.PressRelease import PressRelease
+from utils_future import Parse, WebPage
+
+log = Log("PagePR")
+
+
+class PagePR(WebPage):
+
+    def __gen_div_date_list__(self):
+        div_list = self.soup.find_all(
+            "div",
+            attrs={"data-element_type": "container"},
+        )
+        for div in div_list:
+            h3_list = div.find_all("h3", class_="elementor-icon-box-title")
+            if len(h3_list) != 1:
+                continue
+            yield div, h3_list[0]
+
+    @staticmethod
+    def __parse_div_date_list__(div, h3):
+        date_str = h3.get_text(strip=True)
+        h5_list = div.find_all("h5")
+        for h5 in h5_list:
+            time_str = h5.get_text(strip=True)
+            time = Parse.time(
+                f"{date_str} {time_str}".replace("hrs.", "")[:15]
+            )
+
+            a = h5.find("a")
+            if not a:
+                continue
+            url_pdf = a["href"]
+            assert url_pdf.lower().endswith(".pdf")
+            pr = PressRelease(time=time, url_pdf=url_pdf)
+            yield pr
+
+    @staticmethod
+    def __dedupe_and_sort_pr_list__(pr_list):
+        idx = {pr.time: pr for pr in pr_list}
+        pr_list = list(idx.values())
+        pr_list.sort(key=lambda pr: pr.time)
+        return pr_list
+
+    def get_pr_list(self) -> list[PressRelease]:
+        pr_list = []
+        for div, h3 in self.__gen_div_date_list__():
+            pr_list.extend(self.__parse_div_date_list__(div, h3))
+
+        pr_list = self.__dedupe_and_sort_pr_list__(pr_list)
+        log.info(f"[{self}] Extracted {len(pr_list)} press releases")
+        return pr_list
+
+    def __get_labelled_page__(self, label):
+        a_list = self.soup.find_all("a")
+        for a in a_list:
+            span = a.find("span", text=label)
+            if span:
+                return PagePR(a["href"])
+        log.warning(f"[{self}] '{label}' link not found")
+        return None
+
+    def get_more_page(self) -> "PagePR":
+        return self.__get_labelled_page__("More")
+
+    def get_prev_page(self) -> "PagePR":
+        return self.__get_labelled_page__("Previous")
