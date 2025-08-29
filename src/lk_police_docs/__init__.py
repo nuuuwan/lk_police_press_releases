8548f893cd2348225dcbdccf042f47e28098ed28
nuuuwan <nuuuwan@gmail.com>
2025-08-29 08:38:54 +0530

    deleted python

--- src/lk_police_docs/__init__.py
+++
@@ -1,4 +0,0 @@
-# lk_police_docs (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from lk_police_docs.Scraper import Scraper
--- tests/test_scraper.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from lk_police_docs import Scraper
-
-
-class TestCase(unittest.TestCase):
-    def test_run(self):
-        Scraper(max_delta_t=1).run()
