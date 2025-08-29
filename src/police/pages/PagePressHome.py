09bb99d9d80724568ab34fa7d12e11b29b0fc567
nuuuwan <nuuuwan@gmail.com>
2025-08-29 11:17:20 +0530

    spider by time limit

--- src/police/pages/PagePressHome.py
+++ src/police/pages/PagePressHome.py
@@ -1,3 +1,4 @@
+import time
 from queue import Queue
 
 from utils import Log
@@ -21,27 +22,37 @@ class PagePressHome(WebPage):
     def get_latest_pr_page(self):
         return PagePR(self.__get_latest_pr_page_url__())
 
-    def spider(self, limit):
-        log.info(f"Starting 🕷️ spider with {limit=}")
+    def spider(self, max_dt):
+        log.info(f"Starting 🕷️ spider with {max_dt=}s")
         page = self.get_latest_pr_page()
         page_queue = Queue()
         page_queue.put(page)
         visited_pages = set()
-        pr_list = []
-        while page_queue.not_empty and len(pr_list) < limit:
+        press_release_list = []
+        t_start = time.time()
+        while True:
+            dt = time.time() - t_start
+
+            if page_queue.empty():
+                log.info("🛑 No more pages to visit, Stopping 🕷️ spider")
+                break
+
+            if dt > max_dt:
+                log.info(
+                    f"🛑 Reached {dt:.1f} s> {max_dt}s. Stopping 🕷️ spider"
+                )
+                break
+
             page = page_queue.get()
             visited_pages.add(page)
 
             log.debug(f"Scraping {page}")
-            pr_list_for_page = page.get_pr_list()
-            pr_list.extend(pr_list_for_page)
+            press_release_list_for_page = page.get_press_release_list()
+            press_release_list.extend(press_release_list_for_page)
 
             for page in [page.get_more_page(), page.get_prev_page()]:
                 if page and page not in visited_pages:
                     page_queue.put(page)
 
-        for pr in pr_list:
-            pr.write()
-
-        log.info(f"🕷️ Spidered {len(pr_list)} press releases")
-        return pr_list
+        log.info(f"🕷️ Spidered {len(press_release_list)} press releases")
+        return press_release_list
