3c7f90d6bea1099333a88b3bed64e9fcd1858780
nuuuwan <nuuuwan@gmail.com>
2025-09-05 09:45:17 +0530

    Moved tests, Fixed test_get_latest_pr_page

--- tests/police/pages/test_page_press_home.py
+++ tests/police/pages/test_page_press_home.py
@@ -5,7 +5,7 @@ from police import PagePressHome
 
 class TestCase(unittest.TestCase):
     def test_get_latest_pr_page(self):
-        self.assertEqual(
+        self.assertGreater(
             PagePressHome().get_latest_pr_page().url,
             "https://www.police.lk/?p=11837",
         )
