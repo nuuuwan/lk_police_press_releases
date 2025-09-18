0eca03dc72d922cd8bb96d63e80352906e585cbe
nuuuwan <nuuuwan@gmail.com>
2025-09-18 10:58:16 +0530

    flatten

--- src/police/__init__.py
+++ src/police/__init__.py
@@ -1,9 +1,13 @@
 # police (auto generate by build_inits.py)
 # flake8: noqa: F408
 
-from police.core import (PressRelease, PressReleasePDFDownloaderMixin,
-                         PressReleasePDFTextExtractorMixin,
-                         PressReleaseReadMixin, PressReleaseStatusMixin,
-                         PressReleaseWriteMixin)
-from police.pages import PagePR, PagePressHome
-from police.reports import ReadMe
+from police.PagePR import PagePR
+from police.PagePressHome import PagePressHome
+from police.PressRelease import PressRelease
+from police.PressReleasePDFDownloaderMixin import \
+    PressReleasePDFDownloaderMixin
+from police.PressReleasePDFTextExtractorMixin import \
+    PressReleasePDFTextExtractorMixin
+from police.PressReleaseReadMixin import PressReleaseReadMixin
+from police.PressReleaseStatusMixin import PressReleaseStatusMixin
+from police.PressReleaseWriteMixin import PressReleaseWriteMixin
--- src/police/core/__init__.py
+++
@@ -1,9 +0,0 @@
-# police.core (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from police.core.press_release import (PressRelease,
-                                       PressReleasePDFDownloaderMixin,
-                                       PressReleasePDFTextExtractorMixin,
-                                       PressReleaseReadMixin,
-                                       PressReleaseStatusMixin,
-                                       PressReleaseWriteMixin)
