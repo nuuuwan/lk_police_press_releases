a994c1421783bc0161eb66ccf9d26d7b24795005
nuuuwan <nuuuwan@gmail.com>
2025-09-19 12:57:43 +0530

    Added get_doc_class_description and get_doc_class_emoji

--- src/police/PolicePressRelease.py
+++ src/police/PolicePressRelease.py
@@ -16,6 +16,19 @@ class PolicePressRelease(AbstractPDFDoc):
     LANG = "si"
 
     @classmethod
+    def get_doc_class_description(cls) -> str:
+        return "\n\n".join(
+            [
+                "A police press release is an official public statement issued by the police to inform citizens and the media about incidents, investigations, arrests, public safety alerts, or law enforcement initiatives. It serves as an authoritative source of information, countering rumors and ensuring transparency in policing.",  # noqa: E501
+                "In Sri Lanka, police press releases are especially relevant because they provide timely updates on security matters, traffic advisories, crime prevention campaigns, and major national events. Given the country’s sensitivity to issues of law, order, and public trust, these releases play a key role in shaping public awareness and confidence in the justice system.",  # noqa: E501
+            ]
+        )
+
+    @classmethod
+    def get_doc_class_emoji(cls) -> str:
+        return "👮‍♂️"
+
+    @classmethod
     def gen_docs(cls) -> Generator["PolicePressRelease", None, None]:
         home_page = PolicePressReleaseHomePage()
         for d in home_page.gen_dicts():
